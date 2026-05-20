4b140bb5987d73f75fbcd0675c788e209ffbbbfb
gunnkrrish <gunnkhurana8@gmail.com>
2026-05-20 23:33:19 +0530

    Removed env from tracking

--- TRAVEL-STORY/backend/index.js
+++ TRAVEL-STORY/backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 // const config = require("./config.json");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const cors = require("cors");
 
